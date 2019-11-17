fb9e6b03a4820384039e348de651affd9bb881df
Lavínia <lavininhamelo@hotmail.com>
2019-11-17 02:21:08 -0400

    Initial project structure

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+	dialect: 'postgres',
+	host: 'localhost',
+	username: 'postgres',
+	database: 'postgres',
+	password: 'dbpg',
+	define: {
+		timestamps: true,
+		underscored: true,
+		underscoredAll: true,
+	},
+};
---
+++ src/server.js
@@ -0,0 +1,3 @@
+import app from './app';
+const port = 3333;
+app.listen(port);
