dbdfbe616b6f491327259dcf495896f6fcc9505c
Lavínia <lavininhamelo@hotmail.com>
2019-11-17 03:53:31 -0400

    User Update and Auth Middleware

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
 import UserController from './app/Controllers/UserController';
 import SessionController from './app/Controllers/SessionController';
+import authMiddlewares from './app/Middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
+
+// Router Authorization
+routes.use(authMiddlewares);
+
+routes.put('/users', UserController.update);
 
 export default routes;
